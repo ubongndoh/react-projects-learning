102d0bfbbb010d913a6df39db9b3e9693d53e56d
Ubong Ndoh <ubongndoh@gmail.com>
2021-01-05 23:21:30 +0100

    set up the third project

---
+++ 3-reviews/src/Review.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+const Review = () => {
+	return (
+		<div>
+			<h2>loew</h2>
+		</div>
+	);
+};
+
+export default Review;
